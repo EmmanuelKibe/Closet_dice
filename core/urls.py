529eb3d438efa2979d82ffa599c9013ae5ce4c05
EmmanuelKibe <emmanuelkibe148@gmail.com>
2026-06-11 14:54:35 +0300

    Route urls

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'), 
     path('accounts/register/', views.register, name='register'),
+    path('closet/add/', views.upload_clothing, name='upload_clothing'),
 ]
